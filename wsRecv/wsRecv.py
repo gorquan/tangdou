8e98bf2223790b3a3233c55bdf360c9075f79fa3
gorquan <gorquanwu@gmail.com>
2019-07-23 18:06:20 +0800

    fix some bug in formatmessage function

--- wsRecv/wsRecv.py
+++ wsRecv/wsRecv.py
@@ -11,7 +11,6 @@ from MQBase import MQBase
 import pika
 import html
 
-
 log = ''
 
 Config = ''
@@ -64,7 +63,8 @@ def outputMessage(ws, message):
         log.info("format message success! the message is %s" % message)
         Thread(target=producer, args=(str(message), )).start()
         log.debug(
-            "recive the message, start a thread to save in the rabbitmq server")
+            "recive the message, start a thread to save in the rabbitmq server"
+        )
     except Exception as e:
         log.error("cant start the thread, the reason is %s" % e)
 
@@ -81,7 +81,9 @@ def outputClose(ws):
 def initConnect():
     try:
         websocket.enableTrace(True)
-        ws = websocket.WebSocketApp("ws://" + Config['websocket']['host'] + ':' + str(Config['websocket']['port']) + Config['websocket']['uri'],
+        ws = websocket.WebSocketApp("ws://" + Config['websocket']['host'] +
+                                    ':' + str(Config['websocket']['port']) +
+                                    Config['websocket']['uri'],
                                     on_message=outputMessage,
                                     on_error=outputError,
                                     on_close=outputClose)
@@ -96,14 +98,17 @@ class MQSender(MQBase):
         def sendMessage():
             try:
                 self.open_channel()
-                self.channel.basic_publish(
-                    exchange=self.exchange, routing_key=route, body=msg, properties=self.proprties)
+                self.channel.basic_publish(exchange=self.exchange,
+                                           routing_key=route,
+                                           body=msg,
+                                           properties=self.proprties)
                 success = True
             except Exception as e:
-                log.error(
-                    "cant save the message to server,the reason is: %s" % e)
+                log.error("cant save the message to server,the reason is: %s" %
+                          e)
                 success = False
             return success
+
         result = sendMessage() or sendMessage()
         if not result:
             self.clear()
@@ -111,8 +116,14 @@ class MQSender(MQBase):
 
 
 def producer(message):
-    sender = MQSender(host=Config['receiveRabbitmq']['host'], port=Config['receiveRabbitmq']['port'], exchange=Config['receiveRabbitmq']['exchange'], exchange_type=Config['receiveRabbitmq']['exchange_type'],
-                      user=Config['receiveRabbitmq']['user'], password=Config['receiveRabbitmq']['password'], virtualhost=Config['receiveRabbitmq']['virtual_host'], queue=Config['receiveRabbitmq']['queue'])
+    sender = MQSender(host=Config['receiveRabbitmq']['host'],
+                      port=Config['receiveRabbitmq']['port'],
+                      exchange=Config['receiveRabbitmq']['exchange'],
+                      exchange_type=Config['receiveRabbitmq']['exchange_type'],
+                      user=Config['receiveRabbitmq']['user'],
+                      password=Config['receiveRabbitmq']['password'],
+                      virtualhost=Config['receiveRabbitmq']['virtual_host'],
+                      queue=Config['receiveRabbitmq']['queue'])
     sender.send(Config['receiveRabbitmq']['queue'], message)
 
 
@@ -133,60 +144,105 @@ def formatMessage(message):
         if messagebody.startswith('[CQ'):
             messagebody = messagebody.lstrip('[')
             messagebody = messagebody.rstrip(']')
-            if messagebody.startswith('CQ:rich'):
-                if messagebody.startswith('CQ:rich,content='):
-                    messageJson['msg_body_type'] = 'Shared'
-                    messagebody = (messagebody.split(',content='))[1]
-                    messagebody = (messagebody.split(',title='))[0]
-                    messageJson['message'] = {"SharedTitle": (messagebody.split(',desc:'))[0].lstrip("{news:{title:"), "SharedTag":
-                                              (((messagebody.split(',tag:'))[1]).split(',jumpUrl'))[0], "SharedJumpurl": (((messagebody.split(',jumpUrl:'))[1]).split(',appid:'))[0]}
-                # 文档
-                elif messagebody.startswith('CQ:rich,text='):
-                    messageJson['msg_body_type'] = 'Document'
-                    messagebody = messagebody.split(',')
-                    messageJson['message'] = {"DocumentText": messagebody[1].lstrip(
-                        'text='), "DocumentUrl": messagebody[2].lstrip('url=')}
+            # APP分享
+            if messagebody.startswith('CQ:rich,content='):
+                messageJson['msg_body_type'] = 'Shared'
+                messagebody = (messagebody.split(',content='))[1]
+                messagebody = (messagebody.split(',title='))[0]
+                messageJson['message'] = {
+                    "SharedTitle": (messagebody.split(',desc:')
+                                    )[0].lstrip("{news:{title:"),
+                    "SharedTag": (((messagebody.split(',tag:'))[1]
+                                    ).split(',jumpUrl'))[0],
+                    "SharedJumpurl": (((messagebody.split(',jumpUrl:'))[1]
+                                        ).split(',appid:'))[0]
+                }
+            # 文档
+            elif messagebody.startswith('CQ:rich,text='):
+                messageJson['msg_body_type'] = 'Document'
+                messagebody = messagebody.split(',')
+                messageJson['message'] = {
+                    "DocumentText": messagebody[1].lstrip('text='),
+                    "DocumentUrl": messagebody[2].lstrip('url=')
+                }
             # 图片
             elif messagebody.startswith('CQ:image'):
                 messageJson['msg_body_type'] = 'Image'
                 messagebody = messagebody.split(',')
-                messageJson['message'] = {"ImageFile": messagebody[1].lstrip(
-                    'file='), "ImageUrl": messagebody[2].lstrip('url=')}
+                messageJson['message'] = {
+                    "ImageFile": messagebody[1].lstrip('file='),
+                    "ImageUrl": messagebody[2].lstrip('url=')
+                }
             # 语音
             elif messagebody.startswith('CQ:record'):
                 messageJson['msg_body_type'] = 'Record'
                 messagebody = messagebody.split(',')
+                if len(messagebody) == 3:
+                    RecordMagic = True
+                else:
+                    RecordMagic = False
                 messageJson['message'] = {
-                    "RecordFile": messagebody[1].lstrip('file=')}
-            # 大头贴
-            elif messagebody.startswith('CQ:face') or messagebody.startswith('CQ:bface'):
+                    "RecordFile": messagebody[1].lstrip('file='),
+                    "RecordMagic": RecordMagic
+                }
+            # 表情
+            elif messagebody.startswith('CQ:face'):
                 messageJson['msg_body_type'] = 'Face'
+                faceId = {}
+                if '][' in messagebody:
+                    messagebodys = messagebody.split('][')
+                    for i in len(messagebodys):
+                        messagebody = (messagebodys[i]).split(',')
+                        faceId[i] = messagebody[1].lstrip('id=')
+                else:
+                    messagebody = messagebody.split(',')
+                    faceId[0] = messagebody[1].lstrip('id=')
+                messageJson['message'] = {"FaceId": faceId}
+            # 官方表情包
+            elif messagebody.startswith('CQ:bface'):
+                messageJson['msg_body_type'] = 'Bface'
                 messagebody = messagebody.split(',')
                 messageJson['message'] = {
-                    "FaceId": messagebody[1].lstrip('id=')}
+                    "FaceId": messagebody[1].lstrip('id='),
+                    "FaceP": messagebody[2].lstrip('p=')
+                }
             # 戳一戳
             elif messagebody.startswith('CQ:shake'):
                 messageJson['msg_body_type'] = 'Shake'
                 messagebody = messagebody.split(',')
+                if len(message) == 3:
+                    shakeType = (messagebody[2]).lstrip('type=')
+                else:
+                    shakeType = 'default'
                 messageJson['message'] = {
-                    "ShakeId": (messagebody[1]).lstrip('id=')}
+                    "ShakeId": (messagebody[1]).lstrip('id='),
+                    'ShakeType': shakeType
+                }
             # 地理位置
             elif messagebody.startswith('CQ:location'):
                 messageJson['msg_body_type'] = 'Location'
                 messagebody = messagebody.split(',')
-                messageJson['message'] = {"LocationContent": (messagebody[1]).lstrip('content='), "LocationLat": (
-                    messagebody[2]).lstrip('lat='), "LocationLon": (messagebody[3]).lstrip('lon=')}
+                messageJson['message'] = {
+                    "LocationContent": (messagebody[1]).lstrip('content='),
+                    "LocationLat": (messagebody[2]).lstrip('lat='),
+                    "LocationLon": (messagebody[3]).lstrip('lon='),
+                    "LocationStyle": (messagebody[4]).lstrip('style=')
+                }
             # 名片
             elif messagebody.startswith('CQ:contact'):
                 messageJson['msg_body_type'] = 'Contact'
                 messagebody = messagebody.split(',')
-                messageJson['message'] = {"ContactId": (messagebody[1]).lstrip(
-                    'id='), "ContactType": (messagebody[2]).lstrip('type=')}
+                messageJson['message'] = {
+                    "ContactId": (messagebody[1]).lstrip('id='),
+                    "ContactType": (messagebody[2]).lstrip('type=')
+                }
+            # 厘米秀
             elif messagebody.startswith('CQ:show'):
                 messageJson['msg_body_type'] = 'Show'
                 messagebody = messagebody.split(',')
                 messageJson['message'] = {
-                    'ShowId': (messagebody[2]).lstrip('id=')}
+                    'ShowId': (messagebody[2]).lstrip('id=')
+                }
         # 其他不支持的信息视为文本信息
         else:
             messageJson['msg_body_type'] = 'Text'
