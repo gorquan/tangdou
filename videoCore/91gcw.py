f308533c1b51925132b94983f86b749f0da11c39
gorquan <gorquanwu@gmail.com>
2019-08-15 22:31:07 +0800

    add 91gcw video class

--- videoCore/91gcw.py
+++ videoCore/91gcw.py
@@ -3,41 +3,40 @@
 
 import requests
 from headers import headers
-from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 
-class tangdou(object):
-    """获取糖豆广场舞视频类
+class gcw91(object):
+    """获取就爱广场舞视频类
     :param object: object
     """
     def __init__(self,url):
-        """tangdou class init
-        :param self: self
+        """gcw91 class  init
         :param url: 视频页面地址
         """
-        self.headers = None
         self.url = url
+        self.headers = None
         self.videoUrl = None
         self.success = False
-
+    
     def __getvideoUrl(self):
-        """解析网页获取视频地址
+        """解析页面获取视频地址
         :param self: self
-        """   
+        """
         urlResp = requests.get(self.url, headers=self.headers)
         if urlResp.status_code == 200:
-            page = BeautifulSoup(urlResp.text, 'lxml')
+            page = BeautifulSoup(urlResp.text,'lxml')
             self.videoUrl = (page.find('video')).get('src')
             self.success = True
         else:
             self.videoUrl = None
 
+    
     def getVideoUrl(self):
         """获取视频地址
         :param self: self
         :param return: 返回视频地址videoUrl和解析状态success
-        """   
+        """
         self.headers = headers(self.url).buildHeader()
         self.__getvideoUrl()
         return {'success': self.success, 'videoUrl': self.videoUrl}
